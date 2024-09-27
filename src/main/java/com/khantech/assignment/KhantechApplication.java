dde774f1e14b42f9217a9f66d27c8ddd437fbc85
Danylo Zatorsky <zatorsky.danylo@gmail.com>
2024-09-27 15:39:41 +0300

    Initial Commit

---
+++ src/main/java/com/khantech/assignment/KhantechApplication.java
@@ -0,0 +1,13 @@
+package com.khantech.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhantechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KhantechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/khantech/assignment/TestKhantechApplication.java
@@ -0,0 +1,11 @@
+package com.khantech.assignment;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestKhantechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(KhantechApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
